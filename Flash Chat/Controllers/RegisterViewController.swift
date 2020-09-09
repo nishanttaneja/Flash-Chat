988f60bc75160d543f080a3018015c69f2f81e99
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-09 12:32:46 +0530

    add GoogleServices

--- Flash Chat/Controllers/RegisterViewController.swift
+++ Flash Chat/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
     // IBOutlets
@@ -17,5 +18,30 @@ class RegisterViewController: UIViewController {
     var chatMan = ChatManager()
     
     // IBAction
-    @IBAction func registerButtonPressed(_ sender: UIButton) {}
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        if let email=emailTextField.text, let password=passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                if let errorWhileCreatingUser=error {print(errorWhileCreatingUser); return}
+                else {self.performSegue(withIdentifier: "RegisterToChatScene", sender: self)}
+            }
+        }
+    }
+    
+    //MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        prepareForSegue(segue)
+    }
+}
+
+
+extension UIViewController {
+    //MARK:- Navigation
+    func prepareForSegue(_ segue: UIStoryboardSegue) {
+        switch segue.identifier {
+        case "RegisterToChatScene": print("registered new user and performing segue with identifier 'RegisterToChatScene'")
+        case "LoginToChatScene": print("logged in and performing segue with identifier 'LoginToChatScene'")
+        default:
+            print("unidentified segue identifier found!")
+        }
+    }
 }
