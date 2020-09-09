21601e1ce54ceaa00a90d1b63dde2ee5f9040b22
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-09 12:54:21 +0530

    attach and implement LogOut Action

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -7,24 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // IBAction
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        do {
+            try Auth.auth().signOut();
+            performSegue(withIdentifier: "ChatToWelcomeScene", sender: self)
+        }
+        catch {print(error)}
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        prepareForSegue(segue)
     }
-    */
-
 }
